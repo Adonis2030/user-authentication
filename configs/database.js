0b2563a35088803e0271df318f6e0508d80eecee
Adonis2030 <service.provider2030@gmail.com>
2023-07-10 06:06:16 +0300

    feat: update env file

--- configs/database.js
+++ configs/database.js
@@ -4,11 +4,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // Retrieve environment variables
-const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_CLUSTER } = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_CLUSTER, MONGODB_URL } =
+  process.env;
 
 // Construct the MongoDB connection URL
 const database_url = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/${DB_DATABASE}?retryWrites=true&w=majority`;
 // const database_url = `mongodb://127.0.0.1:27017/soultrain`;
+// const database_url = MONGODB_URL;
 
 // Define the function to connect to the database
 const connectDB = async () => {
